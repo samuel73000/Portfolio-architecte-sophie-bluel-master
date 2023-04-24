91349ba732c7156051b08f94c5a9af50acb07521
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-04-24 10:43:16 +0200

    Portfolio-architecte-sophie-bluel-master

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -7,23 +7,24 @@ submit.addEventListener('submit' ,async (event) => {
     event.preventDfault();
 // envoyer les info de connexion a l'api pour authentification
 const response = await fetch('http://localhost:5678/api/users/login',{
+    
 method:'POST',
     headers:{
     'Content-Type': 'application/json;'
-},
+    },
     body: JSON.stringify(user)
 });
 
 if (response.ok){
     //recuperer le token depuis l'api
     const token = await reponse.json();
-
+    
 //enregistrer le token dans le stockage local
     localStorage.setItem('token',token);
-
+    
 //redirige l'utilisateur vers la page d'accueill
     window.location.href='index.html';
-
+    
 }else{
     //Afficher un message d'erreur si les info de connexion sont invalides
     error.textContent = 'Nom d\'utilisateur ou mot de passe incorrect';
@@ -34,13 +35,14 @@ if (response.ok){
 
 function updateLoginButton(){
     const loginButton = document.querySelector('.lienlogin');
-    const tokken = localStorage.getItem('token');
+    const token = localStorage.getItem('token');
     const isLogged = () => (tokken ? true : false);
-
+   
     if(isLogged){
         loginButton.innerHTML='Logout';
     }else {
         loginButton.innerHTML='Login'
     }
+    
 }
 updateLoginButton();
