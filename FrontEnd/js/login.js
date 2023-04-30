ce41f119e434c3aea2c4b4166e2e0605c8ee8e86
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2023-04-30 12:12:42 +0200

    Portfolio-architecte-sophie-bluel-master

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,48 +1,66 @@
-// recuperation des inputs email et mdp et btn submit
-let user = document.querySelector("#email mdp");
-let error = document.querySelector("#error");
-let submit = document.querySelector("#submit");
-// addeventlistener du btn submit
-submit.addEventListener('submit' ,async (event) => {
-    event.preventDfault();
-// envoyer les info de connexion a l'api pour authentification
-const response = await fetch('http://localhost:5678/api/users/login',{
-    
-method:'POST',
-    headers:{
-    'Content-Type': 'application/json;'
-    },
-    body: JSON.stringify(user)
-});
 
-if (response.ok){
-    //recuperer le token depuis l'api
-    const token = await reponse.json();
-    
-//enregistrer le token dans le stockage local
-    localStorage.setItem('token',token);
-    
-//redirige l'utilisateur vers la page d'accueill
-    window.location.href='index.html';
-    
-}else{
-    //Afficher un message d'erreur si les info de connexion sont invalides
-    error.textContent = 'Nom d\'utilisateur ou mot de passe incorrect';
-}
+const loginForm = document.querySelector('form');
+const emailInput = document.querySelector('input[name="email"]');
+const passwordInput = document.querySelector('input[name="psw"]');
 
-});
+// Écoute de l'événement de soumission du formulaire de connexion
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Empêche l'envoi du formulaire par défaut
 
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-function updateLoginButton(){
-    const loginButton = document.querySelector('.lienlogin');
-    const token = localStorage.getItem('token');
-    const isLogged = () => (tokken ? true : false);
-   
-    if(isLogged){
-        loginButton.innerHTML='Logout';
-    }else {
-        loginButton.innerHTML='Login'
+  // Envoi des données d'identification à l'API pour vérification
+  
+    const response = fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ email, password })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erreur dans l’identifiant ou le mot de passe');
     }
-    
+    return response.json();
+  })
+  .then(data => {
+    if (data.token) {
+      // Stockage du token dans le stockage local
+      localStorage.setItem('token', data.token);
+
+      // Redirection vers la page d'accueil
+      window.location.href = './index.html';
+    } else {
+      throw new Error('Erreur lors de la connexion');
+    }
+  })
+  .catch(error => {
+    console.error('Erreur lors de la connexion:', error);
+    alert(error.message);
+  });
+  
+});
+
+
+/* Login et Logout*/
+
+const token = localStorage.getItem('token');
+function viderToken(){
+  localStorage.removeItem('token');
+  logout.style.display = 'none';
+  login.style.display = 'block';
+  location.reload();
+ }
+
+if (typeof token === 'string' && token.length > 0){
+
+  const logout = document.querySelector(".logout");
+  const login = document.querySelector(".login");
+  logout.style.display = 'block';
+  login.style.display = 'none';
+  logout.addEventListener ('click', function (){
+    viderToken();
+  });
 }
-updateLoginButton();
+
+
